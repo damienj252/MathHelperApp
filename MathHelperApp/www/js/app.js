f57bb262eb9c661b63b505c0d6627a425419c666
[damienj252] <damienj252@gmail.com>
2017-02-24 13:26:38 +0000

    Changed the name of the files and added a log in table as well as changing the tab icons

---
+++ MathHelperApp/www/js/app.js
@@ -0,0 +1,59 @@
+
+angular.module('starter', ['ionic', 'starter.controllers'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+    .state('tab', {
+    url: '/tab',
+    abstract: true,
+    templateUrl: 'templates/tab.html'
+  })
+
+  .state('tab.home', {
+    url: '/home',
+    views: {
+      'tab-home': {
+        templateUrl: 'templates/tab-home.html',
+        controller: 'HomeCtrl'
+      }
+    }
+  })
+
+  .state('tab.logs', {
+      url: '/logs',
+      views: {
+        'tab-logs': {
+          templateUrl: 'templates/tab-logs.html',
+          controller: 'LogsCtrl'
+        }
+      }
+    })
+
+  .state('tab.calculator', {
+    url: '/calculator',
+    views: {
+      'tab-calculator': {
+        templateUrl: 'templates/tab-calculator.html',
+        controller: 'CalculatorCtrl'
+      }
+    }
+  });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/tab/home');
+
+});
---
+++ MathHelperApp/www/js/controllers.js
@@ -0,0 +1,11 @@
+angular.module('starter.controllers', [])
+
+.controller('HomeCtrl', function($scope) {})
+
+.controller('LogsCtrl', function($scope, Chats) {
+  
+})
+
+.controller('CalculatorCtrl', function($scope) {
+  
+});
