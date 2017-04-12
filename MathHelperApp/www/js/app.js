bdd9e4c1152d90143449211f7679843fd6025147
[damienj252] <damienj252@gmail.com>
2017-04-12 21:33:00 +0100

    Attempt at firebase authentication but isn't fully working

--- MathHelperApp/www/js/app.js
+++ MathHelperApp/www/js/app.js
@@ -1,38 +1,74 @@
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'firebase'])
 
-angular.module('starter', ['ionic', 'starter.controllers'])
+.constant('FirebaseUrl', 'https://ionicle.firebaseio.com/')
 
-.run(function($ionicPlatform) {
+.service('rootRef', ['FirebaseUrl', Firebase])
+
+.run(ApplicationRun)
+
+.config(ApplicationConfig);
+
+function ApplicationRun($ionicPlatform, $rootScope, $state) {
   $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
-
     }
     if (window.StatusBar) {
+      
       StatusBar.styleDefault();
     }
   });
-})
 
-.config(function($stateProvider, $urlRouterProvider) {
+  $rootScope.$on('$stateChangeError', function(event, toState, toParams, fromState, fromParams, error) {
+    // We can catch the error thrown when the $requireAuth promise is rejected
+    // and redirect the user back to the home page
+    if (error === 'AUTH_REQUIRED') {
+      $state.go('login');
+    }
+  });
+
+}
+ApplicationRun.$inject = ['$ionicPlatform', '$rootScope', '$state'];
+
+function AuthDataResolver(Auth) {
+  return Auth.$requireAuth();
+}
+AuthDataResolver.$inject = ['Auth'];
+
+function ApplicationConfig($stateProvider, $urlRouterProvider) {
+
   $stateProvider
 
-    .state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'templates/tab.html'
+  .state('login', {
+    url: '/login',
+    templateUrl: 'templates/login.html',
+    controller: 'LoginCtrl as ctrl'
   })
 
-  .state('tab.home', {
-    url: '/home',
-    views: {
-      'tab-home': {
-        templateUrl: 'templates/tab-home.html',
-        controller: 'HomeCtrl'
-      }
+  // setup an abstract state for the tabs directive
+  .state('tab', {
+    url: '/tab',
+    abstract: true,
+    templateUrl: 'templates/tabs.html',
+    resolve: {
+      authData: AuthDataResolver
     }
   })
 
+//Dashboard ---------------------------------------------------------------------------------
+  .state('tab.dashboard', {
+    url: '/dashboard',
+    views: {
+      'tab-dashboard': {
+        templateUrl: 'templates/tab-dashboard.html',
+        controller: 'DashboardCtrl'
+      }
+    }
+  })
+//Logs -------------------------------------------------------------------------------------
   .state('tab.logs', {
       url: '/logs',
       views: {
@@ -42,7 +78,7 @@ angular.module('starter', ['ionic', 'starter.controllers'])
         }
       }
     })
-
+//Calculator -------------------------------------------------------------------------------
   .state('tab.calculator', {
     url: '/calculator',
     views: {
@@ -51,9 +87,10 @@ angular.module('starter', ['ionic', 'starter.controllers'])
         controller: 'CalculatorCtrl'
       }
     }
-  });
+  })
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/home');
+  $urlRouterProvider.otherwise('/login');
 
-});
+}
+ApplicationConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
